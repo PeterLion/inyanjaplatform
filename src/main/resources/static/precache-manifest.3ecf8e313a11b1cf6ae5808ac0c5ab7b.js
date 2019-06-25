4938f63bf8d3d70f55a2b0034b2ae11bd24e5da3
Pierre Celestin NSHIMYUMUREMYI <nshimiyepeter@gmail.com>
2019-06-25 12:21:23 -0600

    Added React App for Client Side

---
+++ src/main/resources/static/precache-manifest.3ecf8e313a11b1cf6ae5808ac0c5ab7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f720ccbc1e5ec5c54c1ba20a74a11aec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88f0b392a4f8b66dd647",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d3fa71cab8bf8b3b2862",
+    "url": "/static/css/main.abdc95a3.chunk.css"
+  },
+  {
+    "revision": "88f0b392a4f8b66dd647",
+    "url": "/static/js/2.e63e7b6b.chunk.js"
+  },
+  {
+    "revision": "d3fa71cab8bf8b3b2862",
+    "url": "/static/js/main.8974d889.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
